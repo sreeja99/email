25364a43a1df38c7300ef7b15ad8d167592f3144
sreeja99 <srijagodishala@gmail.com>
2020-09-29 22:25:25 +0530

    Added:Welcome Statement Functionality Added

---
+++ email/EmailAddress.java
@@ -0,0 +1,11 @@
+package email;
+
+public class EmailAddress {
+
+	public static void main(String[] args) {
+		// welcome statement
+		System.out.println("Welcome to Validate Email with Regex Program");
+
+	}
+
+}
